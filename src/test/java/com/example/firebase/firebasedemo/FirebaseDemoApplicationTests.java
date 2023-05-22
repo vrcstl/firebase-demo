86e9242440ef9843b83e3a7892e91cc8ce73d9fe
vrcstl <133760367+vrcstl@users.noreply.github.com>
2023-05-22 20:44:58 +0700

    123

---
+++ src/test/java/com/example/firebase/firebasedemo/FirebaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firebase.firebasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
