c110593cdffe210000c90729d394911fb966355e
vrcstl <133760367+vrcstl@users.noreply.github.com>
2023-05-22 20:59:54 +0700

    123

--- src/main/java/com/example/firebase/firebasedemo/firebase/FirebaseInitializer.java
+++ src/main/java/com/example/firebase/firebasedemo/firebase/FirebaseInitializer.java
@@ -1,4 +1,29 @@
 package com.example.firebase.firebasedemo.firebase;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Service
 public class FirebaseInitializer {
+    public void Initializer() {
+        FileInputStream serviceAccount =
+                null;
+        try {
+            serviceAccount = new FileInputStream("./serviceAccountKey.json");
+
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
